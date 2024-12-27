b4ae1da133aef49581366ceec5819d05a77d91a1
Swomp03 <robbie.yx.l@gmail.com>
2024-12-27 12:38:47 -0500

    Initial Crypto Project Commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+
+  currentTime: string = new Date().toString().substring(0, 25);;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentTime = new Date().toString().substring(0, 25);
+      // console.log(this.currentTime);
+    }, 1000);
+  }
+
+}
