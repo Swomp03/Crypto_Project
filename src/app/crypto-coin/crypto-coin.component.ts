b2de6517ba2e5424d6200e4560604d88799b1018
Swomp03 <robbie.yx.l@gmail.com>
2025-01-02 17:49:37 -0500

    Angular service start

--- src/app/crypto-coin/crypto-coin.component.ts
+++ src/app/crypto-coin/crypto-coin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { CoinDataService } from '../coin-data.service';
 
 @Component({
   selector: 'app-crypto-coin',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './crypto-coin.component.html',
   styleUrl: './crypto-coin.component.css'
 })
-export class CryptoCoinComponent {
+export class CryptoCoinComponent implements OnInit {
 
+  post: any;
+
+  // constructor(private coinDataService: CoinDataService) { }
+
+  ngOnInit(): void {
+    console.log("Crypto Coin Component Initialized");
+    // this.getData();
+    // if (this.post) {
+    //   console.log("Result:", this.post);
+    // }
+  }
+
+  // getData(): void {
+  //   this.coinDataService.getPosts().subscribe({
+  //     next: (data) => (this.post = data),
+  //     error: (err) => console.error('Error fetching posts:', err),
+  //   });
+  // }
 }
