4d20255594bb5964cfca2ce723d4a77f8cbb9c37
Swomp03 <robbie.yx.l@gmail.com>
2025-01-02 17:14:05 -0500

    Styling for crypto coin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { CryptoCoinComponent } from "./crypto-coin/crypto-coin.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CryptoCoinComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
